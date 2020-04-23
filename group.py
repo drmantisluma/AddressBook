5395f6081c84533b2a7ae641a632cd1107f66594
yudin.k.a <yudin.k.a@effective-group.ru>
2020-04-24 02:46:58 +0300

    Meet the CLASS

---
+++ group.py
@@ -0,0 +1,5 @@
+class Group:
+    def __init__(self, name, foot, head):
+        self.name = name
+        self.foot = foot
+        self.head = head
--- main.py
+++ main.py
@@ -2,20 +2,24 @@
 import unittest
 
 from selenium import webdriver
-from selenium.common.exceptions import NoAlertPresentException
-from selenium.common.exceptions import NoSuchElementException
+
+from group import Group
 
 
 class GroupCreate(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
-        self.verificationErrors = []
-        self.accept_next_alert = True
 
     def test_group_create(self):
-        driver = self.driver
+        wd = self.driver
+        self.login(wd)
+        self.open_group_page(wd)
+        self.open_group_creation(wd)
+        self.fill_group_form(wd, Group("Name", "Head", "Foot"))
+        self.logout(wd)
+
+    def login(self, driver):
         driver.get("http://localhost/addressbook/group.php")
         driver.find_element_by_name("user").click()
         driver.find_element_by_name("user").clear()
@@ -23,48 +27,29 @@ class GroupCreate(unittest.TestCase):
         driver.find_element_by_name("pass").clear()
         driver.find_element_by_name("pass").send_keys("secret")
         driver.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_group_page(self, driver):
         driver.find_element_by_link_text("groups").click()
         driver.find_element_by_name("new").click()
+
+    def open_group_creation(self, driver):
         driver.find_element_by_name("group_name").click()
         driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys("NewGroup")
+
+    def fill_group_form(self, driver, group):
+        driver.find_element_by_name("group_name").send_keys(group.name)
         driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys("GroupHead")
+        driver.find_element_by_name("group_header").send_keys(group.head)
         driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys("GroupFoot")
+        driver.find_element_by_name("group_footer").send_keys(group.foot)
         driver.find_element_by_name("submit").click()
+
+    def logout(self, driver):
         driver.find_element_by_link_text("group page").click()
         driver.find_element_by_link_text("Logout").click()
 
-    def is_element_present(self, how, what):
-        try:
-            self.driver.find_element(by=how, value=what)
-        except NoSuchElementException as e:
-            return False
-        return True
-
-    def is_alert_present(self):
-        try:
-            self.driver.switch_to_alert()
-        except NoAlertPresentException as e:
-            return False
-        return True
-
-    def close_alert_and_get_its_text(self):
-        try:
-            alert = self.driver.switch_to_alert()
-            alert_text = alert.text
-            if self.accept_next_alert:
-                alert.accept()
-            else:
-                alert.dismiss()
-            return alert_text
-        finally:
-            self.accept_next_alert = True
-
     def tearDown(self):
         self.driver.quit()
-        self.assertEqual([], self.verificationErrors)
 
 
 if __name__ == "__main__":
