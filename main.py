bdf4e97becc01f23cadda527451746d64fdb0e06
yudin.k.a <yudin.k.a@effective-group.ru>
2020-04-24 03:10:56 +0300

    + User creation w/ separated class

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import unittest
 from selenium import webdriver
 
 from group import Group
+from user import User
 
 
 class GroupCreate(unittest.TestCase):
@@ -20,7 +21,7 @@ class GroupCreate(unittest.TestCase):
         self.logout(wd)
 
     def login(self, driver):
-        driver.get("http://localhost/addressbook/group.php")
+        driver.get("http://localhost/addressbook/")
         driver.find_element_by_name("user").click()
         driver.find_element_by_name("user").clear()
         driver.find_element_by_name("user").send_keys("admin")
@@ -46,6 +47,54 @@ class GroupCreate(unittest.TestCase):
 
     def logout(self, driver):
         driver.find_element_by_link_text("group page").click()
+        driver.find_element_by_link_text("Logout").click()
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+class UserCreate(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+
+    def test_user_create(self):
+        wd = self.driver
+        self.login(wd)
+        self.open_user_creation(wd)
+        self.fill_user_form(wd, User("First", "Middle", "Last", "Eff-tech", "Nizhniy"))
+        self.logout(wd)
+
+    def login(self, driver):
+        driver.get("http://localhost/addressbook/")
+        driver.find_element_by_name("user").click()
+        driver.find_element_by_name("user").clear()
+        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("pass").clear()
+        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_user_creation(self, driver):
+        driver.find_element_by_link_text("add new").click()
+
+    def fill_user_form(self, driver, user):
+        driver.find_element_by_name("firstname").click()
+        driver.find_element_by_name("firstname").clear()
+        driver.find_element_by_name("firstname").send_keys(user.name)
+        driver.find_element_by_name("middlename").clear()
+        driver.find_element_by_name("middlename").send_keys(user.middle)
+        driver.find_element_by_name("lastname").clear()
+        driver.find_element_by_name("lastname").send_keys(user.last)
+        driver.find_element_by_name("company").click()
+        driver.find_element_by_name("company").clear()
+        driver.find_element_by_name("company").send_keys(user.company)
+        driver.find_element_by_name("address2").click()
+        driver.find_element_by_name("address2").clear()
+        driver.find_element_by_name("address2").send_keys(user.address)
+        driver.find_element_by_xpath("(//input[@name='submit'])[2]").click()
+
+    def logout(self, driver):
+        driver.find_element_by_link_text("home page").click()
         driver.find_element_by_link_text("Logout").click()
 
     def tearDown(self):
---
+++ user.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self, first_name, middle_name, last_name, company, address):
+        self.name = first_name
+        self.middle = middle_name
+        self.last = last_name
+        self.company = company
+        self.address = address
